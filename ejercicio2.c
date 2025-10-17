74aa5d38f3a8c362588c7847584af3feda049496
jona <jona32u4hm@gmail.com>
2025-10-17 11:15:59 -0600

    int main con chequeo de argv

--- ejercicio2.c
+++ ejercicio2.c
@@ -1,4 +1,10 @@
 //ejercicio2.c Jonatán Hidalgo Morales C4G083
 #include <stdio.h>
 
+int main(int argc, char *argv[]){
+    if (argc < 4){
+		printf("uso: %s <Archivo> <Palabra a buscar> <Reemplazo>\n", argv[0]);
+		return 1;
+	}
 
+}
